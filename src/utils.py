296a39a55582bfb40c80e3e54abfa83b9ef29b79
TEJASVEGUPTA <tejasvegupta@gmail.com>
2023-07-14 13:29:17 +0530

    Training Pipeline

--- src/utils.py
+++ src/utils.py
@@ -25,6 +25,7 @@ def save_object(file_path, obj):
 def evaluate_model(X_train,y_train,X_test,y_test,models):
     try:
         report = {}
+        
         for i in range(len(models)):
             model = list(models.values())[i]
             # Train model
